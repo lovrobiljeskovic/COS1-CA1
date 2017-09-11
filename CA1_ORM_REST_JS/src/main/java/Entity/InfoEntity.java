a98d931344fef12a15a99b750a5e7216a4a5df66
mathiasjepsen <mathias@wpsnet.com>
2017-09-11 15:32:31 +0200

    Created InfoEntity entity class

--- CA1_ORM_REST_JS/src/main/java/Entity/InfoEntity.java
+++ CA1_ORM_REST_JS/src/main/java/Entity/InfoEntity.java
@@ -1,6 +1,10 @@
 package Entity;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  *
@@ -8,6 +12,32 @@ import javax.persistence.Entity;
  */
 
 @Entity
-public class InfoEntity {
+public abstract class InfoEntity implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String email;
+
+    public InfoEntity() {
+    }
+    
+    public int getId() {
+        return id;  
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
     
 }
