20c5aa1439d8399f515ff833190dc199504d24d2
Thomas Thimothee <thomas.thimothee@gmail.com>
2017-09-12 10:32:46 +0200

    randomm generator  generator person

--- CA1_ORM_REST_JS/src/main/java/Utility/Generator.java
+++ CA1_ORM_REST_JS/src/main/java/Utility/Generator.java
@@ -1,5 +1,6 @@
 package Utility;
 
+import Entity.Hobby;
 import Entity.Person;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -15,7 +16,7 @@ public class Generator {
     
     private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
     
-    List<String> firstNames = new ArrayList() {
+    private List<String> firstNames = new ArrayList() {
         {
             add("Mathias");
             add("Lovro");
@@ -26,7 +27,7 @@ public class Generator {
         }
     };
     
-    List<String> lastNames = new ArrayList() {
+    private List<String> lastNames = new ArrayList() {
         {
             add("Jepsen");
             add("Biljeskovic");
@@ -36,15 +37,68 @@ public class Generator {
             add("Mihok");
         }
     };
+    
+    private List<Hobby> hobbies = new ArrayList();
+        
+    private void createHobbies(){
+        Hobby h1 = new Hobby();
+        Hobby h2 = new Hobby();
+        Hobby h3 = new Hobby();
+        Hobby h4 = new Hobby();
+        Hobby h5 = new Hobby();
+        Hobby h6 = new Hobby();
+        Hobby h7 = new Hobby();
+        Hobby h8 = new Hobby();
+        Hobby h9 = new Hobby();
+        Hobby h10 = new Hobby();
+        h1.setName("football");
+        h1.setDescription("round ball");
+        h1.setName("handball");
+        h1.setDescription("another round ball but small");
+        h1.setName("video game");
+        h1.setDescription("nerd");
+        h1.setName("painting");
+        h1.setDescription("boring");
+        h1.setName("theatre");
+        h1.setDescription("no comment");
+        h1.setName("reading");
+        h1.setDescription("smart one");
+        h1.setName("reality tv");
+        h1.setDescription("get a life");
+        h1.setName("tennis");
+        h1.setDescription("get a racket");
+        h1.setName("fussball");
+        h1.setDescription("you are an athlete");
+        hobbies.add(h1);
+        hobbies.add(h2);
+        hobbies.add(h3);
+        hobbies.add(h4);
+        hobbies.add(h5);
+        hobbies.add(h6);
+        hobbies.add(h7);
+        hobbies.add(h8);
+        hobbies.add(h9);
+        hobbies.add(h10);        
+    }    
        
     public String generateJSON(int count) {
+        createHobbies();
         Random random = new Random();
         List<Person> persons = new ArrayList();
         for (int i = 0; i < count; i++) {
-            persons.add(new Person(
+            Person p = new Person(
                 firstNames.get(random.nextInt(firstNames.size())), 
-                lastNames.get(random.nextInt(lastNames.size()))));
+                lastNames.get(random.nextInt(lastNames.size())));
             
+            List<Hobby> personHobby =new ArrayList();
+            personHobby.add(hobbies.get(random.nextInt(hobbies.size())));
+            personHobby.add(hobbies.get(random.nextInt(hobbies.size())));
+            
+            while(personHobby.get(0).equals(personHobby.get(1))){
+                personHobby.remove(1);
+                personHobby.add(hobbies.get(random.nextInt(hobbies.size())));
+            }
+            p.setHobbies(personHobby);
         }
         return gson.toJson(persons);
     }
