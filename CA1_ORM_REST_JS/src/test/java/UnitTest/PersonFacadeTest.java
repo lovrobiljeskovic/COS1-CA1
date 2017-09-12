cdb0d1ed7f4d5685687c2798f1f5440d52e42676
mathiasjepsen <mathias@wpsnet.com>
2017-09-12 12:15:25 +0200

    Added a space in a query

--- CA1_ORM_REST_JS/src/test/java/UnitTest/PersonFacadeTest.java
+++ CA1_ORM_REST_JS/src/test/java/UnitTest/PersonFacadeTest.java
@@ -12,7 +12,6 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.assertNotNull;
 /**
  *
  * @author mathiasjepsen
