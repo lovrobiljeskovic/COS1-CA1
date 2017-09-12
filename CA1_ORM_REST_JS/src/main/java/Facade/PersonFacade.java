69f69c0be503a9304c82edd727d5afed8ffc741e
mathiasjepsen <mathias@wpsnet.com>
2017-09-12 08:42:40 +0200

    Added facade interfaces

---
+++ CA1_ORM_REST_JS/src/main/java/Facade/PersonFacade.java
@@ -0,0 +1,58 @@
+package Facade;
+
+import Entity.Person;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author mathiasjepsen
+ */
+public class PersonFacade implements IPersonFacade {
+    
+    private EntityManagerFactory emf;
+    
+    private EntityManager getEntityManager(){ 
+        return emf.createEntityManager();
+    }
+    
+    @Override
+    public void addEntityManagerFactory(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    @Override
+    public Person getPerson(int id) {
+        EntityManager em = getEntityManager();
+        
+        try {
+            
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Person> getPersons() {
+                EntityManager em = getEntityManager();
+        
+        try {
+            
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Person> getPersons(int zipCode) {
+                EntityManager em = getEntityManager();
+        
+        try {
+            
+        } finally {
+            em.close();
+        }
+    }
+    
+}
