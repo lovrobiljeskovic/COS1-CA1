a608490cf55b0006de5ff5db3226b42c4b8054eb
jim <jimtek11@gmail.com>
2017-09-12 12:11:21 +0200

    staged

--- CA1_ORM_REST_JS/src/main/java/RESTperson/PersonResource.java
+++ CA1_ORM_REST_JS/src/main/java/RESTperson/PersonResource.java
@@ -21,7 +21,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
-import utility.JSONcompanyConverter;
+import Utility.JSONcompanyConverter;
 
 /**
  * REST Web Service
