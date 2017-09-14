a2ddcbcbd851b049b3bdfbfc42413c89d1ac7f4c
lovro <cph-lb226@cphbusiness.dk>
2017-09-14 10:13:29 +0200

    created company.js with methods

--- CA1_ORM_REST_JS/src/main/webapp/company.js
+++ CA1_ORM_REST_JS/src/main/webapp/company.js
@@ -14,77 +14,84 @@ getAllCompanies();
 
 var getCompanyByPhone = function () {
     var promise = fetch("http://localhost:8805/CA1_ORM_REST_JS/api/company/complete/phone/" + document.getElementById("phoneNumber").value);
-     promise.then(function (response) {
+    promise.then(function (response) {
         return response.json();
-    }).then(function (companies) {
-        var mappedCompanies = companies.map(function (company) {
-            return "<tr><td>" + company.name + "</td>" + "<td>" + company.description + "</td>" + "<td>" + company.cvr + "</td></tr>";
-        }).join("");
+    }).then(function (company) {
+        var companyByPhone = "<tr><td>" + company.name + "</td>" + "<td>" + company.description + "</td>" + "<td>" + company.cvr + "</td></tr>";
         var tableHeader = "<th>Company Name</th><th>Description</th><th>CVR</th>";
         document.getElementById("phoneHeader").innerHTML = tableHeader;
-        document.getElementById("phoneBody").innerHTML = mappedCompanies;
+        document.getElementById("phoneBody").innerHTML = companyByPhone;
     });
 };
 
 var getCompanyByCVR = function () {
-    var promise = fetch("http://localhost:8805/CA1_ORM_REST_JS/api/company/cvr/" + document.getElementById("cvrNumber"));
-     promise.then(function (response) {
+    var promise = fetch("http://localhost:8805/CA1_ORM_REST_JS/api/company/cvr/" + document.getElementById("cvrNumber").value);
+    promise.then(function (response) {
         return response.json();
-    }).then(function (companies) {
-        var mappedCompanies = companies.map(function (company) {
-            return "<tr><td>" + company.name + "</td>" + "<td>" + company.description + "</td>" + "<td>" + company.cvr + "</td></tr>";
-        }).join("");
-        var tableHeader = "<th>Company Name</th><th>Description</th><th>CVR</th>";
+    }).then(function (company) {
+        var companyByCVR = "<tr><td>" + company.name + "</td>" + "<td>" + company.description + "</td>" + "<td>" + company.cvr + "</td></tr>";
+        var cvrHeader = "<th>Company Name</th><th>Description</th><th>CVR</th>";
         document.getElementById("cvrHeader").innerHTML = cvrHeader;
-        document.getElementById("cvrBody").innerHTML = mappedCompanies;
+        document.getElementById("cvrBody").innerHTML = companyByCVR;
     });
 };
 var getCompanyByID = function () {
-    var promise = fetch("http://localhost:8805/CA1_ORM_REST_JS/api/company/id/" + document.getElementById("idNumber"));
-     promise.then(function (response) {
+    var promise = fetch("http://localhost:8805/CA1_ORM_REST_JS/api/company/id/" + document.getElementById("idNumber").value);
+    promise.then(function (response) {
         return response.json();
-    }).then(function (companies) {
-        var mappedCompanies = companies.map(function (company) {
-            return "<tr><td>" + company.name + "</td>" + "<td>" + company.description + "</td>" + "<td>" + company.cvr + "</td></tr>";
-        }).join("");
+    }).then(function (company) {
+        var CompanyByID = "<tr><td>" + company.name + "</td>" + "<td>" + company.description + "</td>" + "<td>" + company.cvr + "</td></tr>";
         var idHeader = "<th>Company Name</th><th>Description</th><th>CVR</th>";
         document.getElementById("idHeader").innerHTML = idHeader;
-        document.getElementById("idBody").innerHTML = mappedCompanies;
+        document.getElementById("idBody").innerHTML = CompanyByID;
     });
 };
 
 var getCompanyByZipCode = function () {
     var promise = fetch("http://localhost:8805/CA1_ORM_REST_JS/api/company/city/" + document.getElementById("zipCodeNumber"));
-     promise.then(function (response) {
+    promise.then(function (response) {
         return response.json();
-    }).then(function (companies) {
-        var mappedCompanies = companies.map(function (company) {
-            return "<tr><td>" + company.name + "</td>" + "<td>" + company.description + "</td>" + "<td>" + company.cvr + "</td></tr>";
-        }).join("");
+    }).then(function (company) {
+        var CompanyByZip = "<tr><td>" + company.name + "</td>" + "<td>" + company.description + "</td>" + "<td>" + company.cvr + "</td></tr>";
         var zipCodeHeader = "<th>Company Name</th><th>Description</th><th>CVR</th>";
         document.getElementById("zipCodeHeader").innerHTML = zipCodeHeader;
-        document.getElementById("zipCodeBody").innerHTML = mappedCompanies;
+        document.getElementById("zipCodeBody").innerHTML = CompanyByZip;
+    });
+};
+
+var getCompaniesByMinEmployees = function () {
+    var promise = fetch("http://localhost:8805/CA1_ORM_REST_JS/api/company/employees/less/" + document.getElementById("employeeMinNumber").value);
+    promise.then(function (response) {
+        return response.json();
+    }).then(function (companies) {
+         var mappedEmployeeCompanies = companies.map(function (company) {
+            return "<tr><td>" + company.name + "</td>" + "<td>" + company.description + "</td>" + "<td>" + company.cvr + "</td></tr>";
+        }).join("");     
+        var employeeMinHeader = "<th>Company Name</th><th>Description</th><th>CVR</th>";
+        document.getElementById("employeeMinHeader").innerHTML = employeeMinHeader;
+        document.getElementById("employeeMinBody").innerHTML = mappedEmployeeCompanies;
     });
 };
 var postData = function () {
-      var headers = {
-        'Content-Type':'application/json'
+    var headers = {
+        'Content-Type': 'application/json'
     };
     var promise = fetch("http://localhost:8805/CA1_ORM_REST_JS/api/company", {
         method: "POST",
         headers: headers,
         body: JSON.stringify({
             name: document.getElementById('name').value,
-              description: document.getElementById('description').value,
-                cvr: document.getElementById('cvr').value,
-                
+            description: document.getElementById('description').value,
+            cvr: document.getElementById('cvr').value,
+
         })
     }).then(function (response) {
         return response.json();
     });
-    };
-    
-    document.getElementById("phone").addEventListener("click", getCompanyByPhone);
-    document.getElementById("cvr").addEventListener("click", getCompanyByCVR);
-      document.getElementById("id").addEventListener("click", getCompanyByID);
-       document.getElementById("zipCode").addEventListener("click", getCompanyByZipCode);
+};
+
+document.getElementById("phone").addEventListener("click", getCompanyByPhone);
+document.getElementById("cvr").addEventListener("click", getCompanyByCVR);
+document.getElementById("id").addEventListener("click", getCompanyByID);
+document.getElementById("zipCode").addEventListener("click", getCompanyByZipCode);
+document.getElementById("minNumber").addEventListener("click", getCompaniesByMinEmployees);
